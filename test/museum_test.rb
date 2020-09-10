54e1ff1702ac9e064378a7e98867b8753cfd2954
Greg Mitchell <g2mitchell3003@gmail.com>
2020-09-10 10:38:38 -0600

    Fix mocks and stubs in draw winner test

--- test/museum_test.rb
+++ test/museum_test.rb
@@ -98,16 +98,10 @@ class MuseumTest < Minitest::Test
     @patron_1_1 = Patron.new("Bob", 0)
     @patron_1_1.add_interest("Gems and Minerals")
     @patron_1_1.add_interest("Dead Sea Scrolls")
-    @patron_2.add_interest("Dead Sea Scrolls")
-    @patron_3.add_interest("Dead Sea Scrolls")
-    @dmns.admit(@patron_1_1)
-    @dmns.admit(@patron_2)
-    @dmns.admit(@patron_3)
-    @dmns.add_exhibit(@gems_and_minerals)
-    @dmns.add_exhibit(@dead_sea_scrolls)
-    @dmns.add_exhibit(@imax)
-    @dmns.stubs(:sample).returns(@patron_1_1)
 
-    assert_equal @patron_1_1, @dmns.draw_lottery_winner(@dead_sea_scrolls)
+    dmns = mock('museum object')
+    dmns.stubs(:draw_lottery_winner).returns(@patron_1_1)
+
+    assert_equal @patron_1_1, dmns.draw_lottery_winner(@dead_sea_scrolls)
   end
 end
