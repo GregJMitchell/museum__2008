99a689070403b343e96b6737ac12898c0e1f77a3
Greg Mitchell <g2mitchell3003@gmail.com>
2020-09-10 10:21:01 -0600

    Add test for ticket lottery contestants method

--- test/museum_test.rb
+++ test/museum_test.rb
@@ -76,4 +76,19 @@ class MuseumTest < Minitest::Test
                 @imax => []}
     assert_equal expected, @dmns.patrons_by_exhibit_interest
   end
+
+  def test_it_can_find_lottery_contestants
+    @patron_1.add_interest("Gems and Minerals")
+    @patron_1.add_interest("Dead Sea Scrolls")
+    @patron_2.add_interest("Dead Sea Scrolls")
+    @patron_3.add_interest("Dead Sea Scrolls")
+    @dmns.admit(@patron_1)
+    @dmns.admit(@patron_2)
+    @dmns.admit(@patron_3)
+    @dmns.add_exhibit(@gems_and_minerals)
+    @dmns.add_exhibit(@dead_sea_scrolls)
+    @dmns.add_exhibit(@imax)
+
+    assert_equal [@patron_1, @patron_2], @dmns.ticket_lottery_contestants(@dead_sea_scrolls)
+  end
 end
