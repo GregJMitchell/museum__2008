3e3f26e3e752e03bc8d533d040d81d951150db35
Greg Mitchell <g2mitchell3003@gmail.com>
2020-09-10 10:29:07 -0600

    Add test for drawing winner method

--- test/museum_test.rb
+++ test/museum_test.rb
@@ -92,4 +92,20 @@ class MuseumTest < Minitest::Test
 
     assert_equal [@patron_1_1, @patron_3], @dmns.ticket_lottery_contestants(@dead_sea_scrolls)
   end
+
+  def test_it_can_draw_winner
+    @patron_1_1 = Patron.new("Bob", 0)
+    @patron_1_1.add_interest("Gems and Minerals")
+    @patron_1_1.add_interest("Dead Sea Scrolls")
+    @patron_2.add_interest("Dead Sea Scrolls")
+    @patron_3.add_interest("Dead Sea Scrolls")
+    @dmns.admit(@patron_1_1)
+    @dmns.admit(@patron_2)
+    @dmns.admit(@patron_3)
+    @dmns.add_exhibit(@gems_and_minerals)
+    @dmns.add_exhibit(@dead_sea_scrolls)
+    @dmns.add_exhibit(@imax)
+
+    assert_equal @patron_1_1, @dmns.draw_lottery_winner(@dead_sea_scrolls)
+  end
 end
