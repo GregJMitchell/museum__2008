c009c4fde57794b71c37e72bcf0b4bd7d9b57fab
Greg Mitchell <g2mitchell3003@gmail.com>
2020-09-10 09:46:29 -0600

    Code recommend exhibits method and update setup method

--- lib/museum.rb
+++ lib/museum.rb
@@ -8,4 +8,16 @@ class Museum
   def add_exhibit(exhibit)
     @exhibits << exhibit
   end
+
+  def recommend_exhibits(patron)
+    recommenations = []
+    patron.interests.each do |interest|
+      @exhibits.each do |exhibit|
+        if exhibit.name == interest
+          recommenations << exhibit
+        end
+      end
+    end
+    recommenations
+  end
 end
